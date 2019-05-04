c599e894351e9e148b7f9f054e26385cd6b6f06d
JpyChiu <joy4707710@gmail.com>
2019-05-04 11:13:22 +0800

    Initial commit

---
+++ RandomSelector/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#choose_food").click(function()
+    {
+        var numbers_of_li = $("#choices li").length;
+        var random_list_number = Math.floor(Math.random()*numbers_of_li);
+        $("#food_name").text($("#choices li").eq(random_list_number).text());
+        switch(random_list_number)
+            {
+                case 0:
+                    $("#food_img").attr("src", "RandomSelector/image/img1.jpg");
+                    break;
+                case 1:
+                    $("#food_img").attr("src", "RandomSelector/image/img2.webp");
+                    break;
+                case 2:
+                    $("#food_img").attr("src", "RandomSelector/image/img3.jpg");
+                    break;
+            }
+    });
+});
---
+++ YoutubePlayer/playlist.js
@@ -0,0 +1,12 @@
+var playList = [
+    "L229QDxDakU",
+    "kfXdP7nZIiE",
+    "x3bDhtuC5yk"
+];
+
+//播放開始秒數
+var playTime =[
+    [1,5],
+    [1,5],
+    [1,5]
+];
